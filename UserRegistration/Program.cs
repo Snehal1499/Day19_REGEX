370ff4dbed2684b306ef953bac73aa54488a6120
Snehal1499 <snehalpatil1718@gmail.com>
2022-03-31 22:47:36 +0530

    Added UC1 First Name

--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace UserRegistration
 {
@@ -7,6 +8,21 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Regex User Registration Problem");
+            Console.WriteLine("Regex User Registration First Name");
+            Console.WriteLine("Enter first Name");
+
+            string text = Console.ReadLine();
+            //check the given input contain
+
+            if (Regex.Match(text, "^[A-Z][a-z]{3}$").Success)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
         }
     }
 }
+
